8a0257117e552e667b4e7b46662063c1f2f2e4e9
VintageM8 <71400748+VintageM8@users.noreply.github.com>
2022-06-14 12:06:58 -0400

    Element weakness

---
+++ Common/ElementList.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ElementariaMod.Common
+{
+    public class ElementLists
+    {
+        public static List<int> fireEnemies;
+
+        public static void LoadLists()
+        {
+            fireEnemies = new List<int>()
+            {
+                NPCID.LavaSlime,
+                NPCID.VoodoDemon,
+                NPCID.LavaBat,
+                NPCID.RuneWizard,
+                NPCID.RedDevil,
+                NPCID.RaggedCaster,
+                NPCID.Corite,
+                NPCID.Crawltipide,
+                NPCID.Drakomire,
+                NPCID.DrakomireRider,
+                NPCID.Drakanian,
+                NPCID.Selenian,
+                NPCID.Sroller,
+                NPCID.Betsy,
+                NPCID.MourningWood,
+                NPCID.Pumpking,
+                NPCID.SolarPillar,
+            };
+        }
+    }
+}
