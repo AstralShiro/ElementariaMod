6ce06b468ca15c51726333500831ec47463ef580
Shishiko <greenshadow0098@gmail.com>
2022-06-17 20:40:32 +0200

    Scarlet

--- Content/Items/Weapons/Custom/Sword/ScarletSword.cs
+++ Content/Items/Weapons/Custom/Sword/ScarletSword.cs
@@ -28,5 +28,13 @@ namespace ElementariaMod.Content.Items.Weapons.Custom.Sword
             Item.useTurn = false;
             Item.UseSound = SoundID.Item1;
         }
+
+        public override void ModifyHitNPC(Player player, NPC target, ref int damage, ref float knockBack, ref bool crit)
+        {
+            if (ElementLists.fireEnemies.Contains(target.type))
+            {
+                damage = (int)(damage * 1.8f);
+            }
+        }
     }
 }
